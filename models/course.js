f7dda126570a4211c4cb87194cf721d0f7ea648f
Ｎｉｃｋ  Ｒｉｃｃｉ <95265278+niickricci@users.noreply.github.com>
2024-09-24 11:33:21 -0400

    Added course

---
+++ models/course.js
@@ -0,0 +1,12 @@
+import Model from './model.js';
+
+export default class Course extends Model {
+    constructor() {
+        super();
+
+        this.addField('Code', 'string');
+        this.addField('Title', 'string');
+              
+        this.setKey("Code");
+    }
+}
